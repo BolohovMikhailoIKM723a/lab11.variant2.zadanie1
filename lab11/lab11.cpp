8089a8b09310c4163e9147135a8a0e652ad5c991
Михаил <@DESKTOP-1RCLF6R>
2024-04-07 02:31:08 +0300

    Add project files.

---
+++ lab11/lab11.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b;
+
+    cout << "введіть значення для a: ";
+    cin >> a;
+    cout << "введіть значення для b: ";
+    cin >> b;
+    int* ptr_a = &a;
+    int* ptr_b = &b;
+    if (*ptr_a > *ptr_b) {
+        *ptr_a *= 2; 
+    }
+    else {
+        *ptr_b /= 2; 
+    }
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    return 0;
+}
